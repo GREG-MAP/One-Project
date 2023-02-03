f96d9a23041de323d9e9f0e17efc3ffd0d6d5eb1
Greenvalt <100868342+Greenvalt@users.noreply.github.com>
2023-02-03 22:30:28 +0500

    Проработка заготовки ауидо-контроля

---
+++ One-Game/Assets/Scripts/Meny/AudioControll.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioControll : MonoBehaviour
+{
+    public string volumeParameter = "MasterVolume";
+    public AudioMixer mixer;
+    public Slider slider;
+
+    private float _volumeValue;
+    private const float _multiplier = 20f;
+
+    private void Awake()
+    {
+        slider.onValueChanged.AddListener(ValueChange);
+    }
+    private void Start()
+    {
+        _volumeValue = PlayerPrefs.GetFloat(volumeParameter, Mathf.Log10(slider.value) * _multiplier);
+        slider.value = Mathf.Pow(10f, _volumeValue / _multiplier);
+    }
+
+    private void ValueChange(float value)
+    {
+        _volumeValue = Mathf.Log10(value) * _multiplier;
+        mixer.SetFloat(volumeParameter, _volumeValue);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(volumeParameter, _volumeValue);
+    }
+}
