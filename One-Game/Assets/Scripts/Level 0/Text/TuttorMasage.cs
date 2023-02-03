6c3428f9d2f6e62a95b74d6c9cbac56ead570fb4
Greenvalt <100868342+Greenvalt@users.noreply.github.com>
2023-02-03 13:24:02 +0500

    Update TuttorMasage.cs

--- One-Game/Assets/Scripts/Level 0/Text/TuttorMasage.cs
+++ One-Game/Assets/Scripts/Level 0/Text/TuttorMasage.cs
@@ -9,11 +9,27 @@ public class TuttorMasage : MonoBehaviour
     private void Awake()
     {
         _tt.gameObject.GetComponent<CanvasGroup>().alpha = _alphastart;
+        
+    }
+
+    private IEnumerator MasageLoader()
+    {
+        for(_alphastart = 0.0005f; _alphastart < 1.2f; _alphastart += 0.05f)
+        {
+            _tt.gameObject.GetComponent<CanvasGroup>().alpha = _alphastart + _alphastart;
+
+            yield return new WaitForSeconds(0.1f);
+
+            Debug.Log("Wath ?");
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         _tt.gameObject.SetActive(true);
+
+        StartCoroutine(MasageLoader());
     }
 
     private void OnTriggerExit2D(Collider2D collision)
